11636291c8cbae08c9c0a5aa6961b691e71b6780
Jiansong Li <lijianso@usc.edu>
2023-05-01 23:42:58 -0700

    Add files via upload

--- translator.py
+++ translator.py
@@ -1,6 +1,5 @@
 # Using python version 3.10.7
-import json
-import azure.cognitiveservices.speech as speech
+import azure.cognitiveservices.speech as speech # version 1.28.0
 
 
 API_KEY = "2a66aa050dec476e9ede880f7e09e932"
@@ -10,26 +9,22 @@ end_point = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issuetoken"
 media_file_path = './recording1.wav'
 
 # config speech
-'''
-speech_config = speech.SpeechConfig(subscription="2a66aa050dec476e9ede880f7e09e932", endpoint=end_point,
-                                    speech_recognition_language='zh-Hans')
+
+speech_config = speech.SpeechConfig(subscription="2a66aa050dec476e9ede880f7e09e932", endpoint=end_point)
 audio_config = speech.audio.AudioConfig(filename=media_file_path)
-speech_recognizer = speech.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config, language='zh-Hans')
+speech_recognizer = speech.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 result = speech_recognizer.recognize_once_async().get()
 print(result)
-print(result.cancellation_details.error_details)
-# source_language_text = result.text
-# translation = result.translations['en']
-# print(translation)
+
 translation_config = speech.translation.SpeechTranslationConfig(subscription=API_KEY, endpoint=end_point)
-translation_config.speech_recognition_language = 'zh-Hans'
-translation_config.add_target_language('en-US')
+translation_config.speech_recognition_language = 'en-US'
+translation_config.add_target_language('zh-Hans')
 translator = speech.translation.TranslationRecognizer(translation_config=translation_config, audio_config=audio_config)
 result_translated = translator.recognize_once_async().get()
-print(result_translated.translations)
-'''
-# translation config
+print(result_translated.translations['zh-Hans'])
 
+# translation config
+'''
 translation_config = speech.translation.SpeechTranslationConfig(subscription=API_KEY, endpoint=end_point)
 translation_config.speech_recognition_language = 'en-US'
 translation_config.add_target_language('zh-Hans')
@@ -37,3 +32,4 @@ audio_config = speech.audio.AudioConfig(filename=media_file_path) #if nothing in
 recognizer = speech.translation.TranslationRecognizer(translation_config=translation_config, audio_config=audio_config)
 result = recognizer.recognize_once()
 print(result)
+'''
