d8f2658142e94dd65ac9010949f3d953d960a5ac
robertzhang <123130908+robertzhang02@users.noreply.github.com>
2023-05-04 04:03:01 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,22 +4,22 @@ def Main():
     host = '192.168.191.3' # set the server IP address
     port = 5000 # set the port number
 
-    s = socket.socket() # create a socket object
+    s = socket.socket() # create a socket 
     s.bind((host,port)) # bind the socket to the IP address and port number
     s.listen(1) # listen for incoming connections
 
-    print('Server ready to receive data...')
+    print('Server ready to connect...')
 
     conn, addr = s.accept() # accept the connection request
     print('Connection from: ' + str(addr))
 
-    with open('speech.txt', 'rb') as f: # open the file to be transferred in binary mode
+    with open('speech.txt', 'rb') as f: # open the file to be transferred
         data = f.read(1024) # read the file in chunks of 1024 bytes
         while data:
             conn.send(data) # send the data to the client
             data = f.read(1024) # read the next chunk of data
 
-    print('Data transfer complete')
+    print('Data transfer complete')# Remind transfering completion
     conn.close() # close the connection
 
 if __name__ == '__main__':
